99fdb8dd92fe255cf20a3b0a5acde8dfc46d3f5b
panutat-p <pp.panutat@gmail.com>
2023-03-25 23:17:50 +0700

    feat: add Hello component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Hello from "@/components/Hello.vue";
+</script>
 
 <template>
-  <div class="text-4xl flex items-center justify-center h-screen">
-    <section>
-      <h1 class="mb-3">Centered using Tailwind Flex</h1>
-      <div class="flex justify-center"><button class="btn">Click</button></div>
-    </section>
+  <div class="flex flex-col items-center justify-center h-screen">
+    <Hello></Hello>
   </div>
 </template>
---
+++ src/components/Hello.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1 class="text-5xl mb-3">Centered using Tailwind Flex</h1>
+  <div><button class="btn">Click</button></div>
+</template>
