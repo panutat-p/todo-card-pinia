802e49eb396e2e4a3027cec6a18457c6d3ecf682
panutat-p <pp.panutat@gmail.com>
2023-03-27 00:23:57 +0700

    feat: complete EventDetail

---
+++ src/components/BackButton.vue
@@ -0,0 +1,3 @@
+<template>
+  <button class="btn btn-lg" v-on:click="$router.back()">BACK</button>
+</template>
--- src/views/EventDetail.vue
+++ src/views/EventDetail.vue
@@ -1,29 +1,46 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { reactive } from 'vue';
+import { useRoute } from 'vue-router';
+
+import { useEventList } from '@/stores/event-list';
+import { TodoEvent } from '@/types/event.type';
+import BackButton from '@/components/BackButton.vue';
+
+const route = useRoute();
+const event = reactive<TodoEvent>({
+  id: 0,
+  title: '_',
+  date: '2000-01-01',
+  time: '00:00:00',
+  img: 'https://placehold.co/300x300/EEEEEE/white',
+});
+
+const eventListStore = useEventList();
+
+const item = eventListStore.getEventByID(Number(route.params?.id));
+if (item !== undefined) {
+  event.id = item.id;
+  event.title = item.title;
+  event.date = item.date;
+  event.time = item.time;
+  event.img = item.img;
+}
+</script>
 
 <template>
-  <h1 class="text-4xl mt-5 mb-10">Event Detail</h1>
-  <div class="mb-4">
-    <input
-      type="text"
-      placeholder="Type here"
-      class="input input-bordered w-full max-w-xs"
-    />
-    <button class="btn btn-primary">Update</button>
-  </div>
-  <div class="mb-4">
-    <input
-      type="text"
-      placeholder="Type here"
-      class="input input-bordered w-full max-w-xs"
-    />
-    <button class="btn btn-primary">Update</button>
-  </div>
-  <div class="mb-4">
-    <input
-      type="text"
-      placeholder="Type here"
-      class="input input-bordered w-full max-w-xs"
-    />
-    <button class="btn btn-primary">Update</button>
-  </div>
+  <section class="hero min-h-screen bg-base-200">
+    <article class="hero-content flex-col">
+      <div>
+        <h1 class="text-5xl font-bold mb-14">{{ event.title }}</h1>
+        <article class="text-center mb-14">
+          <span class="mr-3 badge badge-outline text-lg text-blue-500">{{ event.date }}</span>
+          <span class="badge badge-outline text-lg text-purple-500">
+            {{ event.time }}
+          </span>
+        </article>
+      </div>
+      <img v-bind:src="event.img" class="max-w-sm rounded-lg shadow-2xl" alt="event-image" />
+      <BackButton class="mt-24"></BackButton>
+    </article>
+  </section>
 </template>
